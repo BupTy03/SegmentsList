a380026fc3d8d947961bcb0ce13db18dd046e1af
Anton <anton.lagosha.99@mail.ru>
2020-02-24 20:07:20 +0500

    Добавьте файлы проекта.

---
+++ SegmentsList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+using BMIList = SegmentsList.SegmentsList<double, string>;
+using AgesBMIList = SegmentsList.SegmentsList<int, SegmentsList.SegmentsList<double, string>>;
+
+
+namespace SegmentsList
+{
+    class Program
+    {
+        static void TestSegmentsList()
+        {
+            BMIList middle = new BMIList();
+            middle.Add(0, "Дефицит массы тела");
+            middle.Add(19.5, "Норма");
+            middle.Add(23, "Избыток массы тела");
+            middle.Add(27.5, "Ожирение 1 степени");
+            middle.Add(30, "Ожирение 2 степени");
+            middle.Add(35, "Ожирение 3 степени");
+            middle.Add(40, "Ожирение 4 степени");
+
+            BMIList old = new BMIList();
+            old.Add(0, "Дефицит массы тела");
+            old.Add(20, "Норма");
+            old.Add(26, "Избыток массы тела");
+            old.Add(28, "Ожирение 1 степени");
+            old.Add(31, "Ожирение 2 степени");
+            old.Add(36, "Ожирение 3 степени");
+            old.Add(41, "Ожирение 4 степени");
+
+            AgesBMIList list = new AgesBMIList();
+            list.Add(18, middle);
+            list.Add(30, old);
+
+            for (int age = 18; age < 100; ++age)
+            {
+                Console.WriteLine("==========   age = {0}   ===========", age);
+                for (double bmi = 0.0; bmi < 50; bmi += 0.5)
+                    Console.WriteLine("BMI = {0}; {1}", bmi, list[age][bmi]);
+                Console.WriteLine("===================================");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TestSegmentsList();
+        }
+    }
+}
---
+++ SegmentsList/SegmentsList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace SegmentsList
+{
+    class SegmentsList<TKey, TValue> where TKey : IComparable
+    {
+        public SegmentsList()
+        {           
+        }
+
+        public bool Add(TKey lowerBound, TValue value)
+        {
+            var segment = new Segment(lowerBound, value);
+            int index = m_data.BinarySearch(segment, new SegmentComp());
+            if(index >= 0)
+                return false;
+
+            index = ~index;
+            m_data.Insert(index, segment);
+            return true;
+        }
+
+        public TValue this[TKey lowerBound] { 
+            get 
+            {
+                if (m_data.Count == 0)
+                    throw new IndexOutOfRangeException();
+
+                var segment = new Segment(lowerBound, default(TValue));
+                int index = m_data.BinarySearch(segment, new SegmentComp());
+                if (index >= 0)
+                    return m_data[index].Value;
+
+                index = ~index;
+                return m_data[index - 1].Value;
+            }
+        }
+
+        private struct Segment
+        {
+            public Segment(TKey lowerBound, TValue value)
+            {
+                LowerBound = lowerBound;
+                Value = value;
+            }
+
+            public TKey LowerBound { get; }
+            public TValue Value { get; }
+        }
+
+        private class SegmentComp : IComparer<Segment>
+        {
+            public int Compare([AllowNull] Segment x, [AllowNull] Segment y)
+            {
+                return x.LowerBound.CompareTo(y.LowerBound);
+            }
+        }
+
+        private List<Segment> m_data = new List<Segment>();
+    }
+}
